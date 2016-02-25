d40c9ec835bdb630dd1994072586cd6680f437e8
Jake Gavin <jakegavin@gmail.com>
2016-02-24 23:49:18 -0800

    chore(dev) add react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
+var webpack = require('webpack')
+
 module.exports = {
-  context: __dirname + "/src",
-  entry: "./index",
+  entry: [
+    "webpack-dev-server/client?http://0.0.0.0:3000",
+    "webpack/hot/only-dev-server",
+    "./src/index",
+  ],
 
   output: {
     filename: "bundle.js",
     path: __dirname + "/dist",
+    publicPath: "/static/"
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ["babel-loader"],
+        loaders: ["react-hot", "babel-loader"],
       }
     ],
   },
